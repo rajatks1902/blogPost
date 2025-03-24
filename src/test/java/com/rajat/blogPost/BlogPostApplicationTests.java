c4e758bcf57eee606081faac072a3e34f6f578d1
rajatks1902 <rajatkumarsingh0707@gmail.com>
2025-03-24 07:50:15 -0700

    first commit

---
+++ src/test/java/com/rajat/blogPost/BlogPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.blogPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
